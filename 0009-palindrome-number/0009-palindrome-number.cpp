4d046ceca7bc039602abb6dee75148ebaeec8117
ANSH SURYAWANSHI <anshsuryawanshi2207@gmail.com>
2026-08-04 00:40:27 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.62%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)
+        return false;
+        int original =x;
+        long long rev =0;
+    while(x!=0){
+        int digit= x%10;
+        x=x/10;
+         rev = rev*10+digit;
+
+    }
+    if(rev==original)
+    return true;
+    else
+    return false;
+
+    }
+};
